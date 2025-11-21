d4e15f20539a3711919676ae1e85d3d61b4bb4ae
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2025-11-21 14:34:27 +0100

    Feat(Exercice): Fini l'exercice Lucky Numbers
    [50] [DONE]

--- exercices/lucky-numbers/lucky-numbers.js
+++ exercices/lucky-numbers/lucky-numbers.js
@@ -8,7 +8,7 @@
  * @returns {number} sum of the two arrays
  */
 export function twoSum(array1, array2) {
-  throw new Error('Remove this line and implement the function');
+  return Number(array1.join('')) + Number(array2.join('')) // Le Join les mets ensemble en string avec aucun espace ('') et ensuite les additionne en nombres. 
 }
 
 /**
@@ -18,7 +18,10 @@ export function twoSum(array1, array2) {
  * @returns {boolean} whether the number is a palindrome or not
  */
 export function luckyNumber(value) {
-  throw new Error('Remove this line and implement the function');
+  let valueString = value.toString();
+  let part1 = valueString.slice(0, Math.ceil(valueString.length / 2));
+  let part2 = valueString.slice(valueString.length / 2, valueString.length).split("").reverse().join("");
+  return (part1 == part2)
 }
 
 /**
@@ -29,5 +32,7 @@ export function luckyNumber(value) {
  * @returns {string} error message
  */
 export function errorMessage(input) {
-  throw new Error('Remove this line and implement the function');
+  if (!input) return 'Required field'
+  else if (Number.isNaN(Number(input)) || String(input).includes('0',)) return 'Must be a number besides 0'
+  return ''
 }
