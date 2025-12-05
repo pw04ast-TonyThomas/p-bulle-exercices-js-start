6b34e7a9f7b2918e493d46da02f2daf44af6f7e3
Tony Thomas <pw04ast@eduvaud.ch>
2025-12-05 16:33:24 +0100

    Feat(Exercice): début de l'exercice Lasagna Master
    [20] [DONE]

--- exercices/lasagna-master/lasagna-master.js
+++ exercices/lasagna-master/lasagna-master.js
@@ -9,4 +9,29 @@
  * export function yourFunction(...) {
  *   ...
  * }
+ * @param {Number} preparationTime
  */
+
+export function cookingStatus(preparationTime) {
+    if (preparationTime == 0){
+        return 'Lasagna is done.'
+    } else if (!preparationTime) {
+        return 'You forgot to set the timer.'
+    } else {return 'Not done, please wait.'}
+}
+
+export function addSecretIngredient() {
+    throw new Error('Remove this line and implement the function');
+}
+
+export function preparationTime() {
+    throw new Error('Remove this line and implement the function');
+}
+
+export function quantities() {
+    throw new Error('Remove this line and implement the function');
+}
+
+export function scaleRecipe() {
+    throw new Error('Remove this line and implement the function');
+}
