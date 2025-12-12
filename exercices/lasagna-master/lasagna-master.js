a1a0687b080fe90ee1da7abcf7932cae955f04e1
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2025-12-12 11:24:46 +0100

    Feat(Exercice): Fini l'exercice Lasagna Master
    [45] [DONE]

--- exercices/lasagna-master/lasagna-master.js
+++ exercices/lasagna-master/lasagna-master.js
@@ -20,18 +20,60 @@ export function cookingStatus(preparationTime) {
     } else {return 'Not done, please wait.'}
 }
 
-export function addSecretIngredient() {
-    throw new Error('Remove this line and implement the function');
+/**
+ * 
+ * @param {Array<string>} friendsList 
+ * @param {Array<string>} myList 
+ */
+
+export function addSecretIngredient(friendsList, myList) {
+    myList.push(friendsList[friendsList.length - 1])
 }
 
-export function preparationTime() {
-    throw new Error('Remove this line and implement the function');
+/**
+ * 
+ * @param {Array<string>} layers 
+ * @param {Number} time 
+ */
+
+export function preparationTime(layers, time) {
+    if (!time) time = 2
+
+    return layers.length * time
 }
 
-export function quantities() {
-    throw new Error('Remove this line and implement the function');
+/**
+ * 
+ * @param {Array<string>} layers 
+ */
+
+export function quantities(layers) {
+    let noodles = 0;
+    let sauce = 0;
+
+    layers.forEach(layer => {
+        if (layer == 'noodles') {
+            noodles += 50
+        } else if (layer == 'sauce') {
+            sauce += 0.2
+        }
+    }) 
+
+    return {noodles, sauce}
 }
 
-export function scaleRecipe() {
-    throw new Error('Remove this line and implement the function');
+/**
+ * 
+ * @param {Object} recipe 
+ * @param {Number} scale 
+ */
+
+export function scaleRecipe(recipe, scale) {
+    const scaled = { ...recipe }
+    const factor = scale / 2
+
+    for (let ingredient in scaled) {
+        scaled[ingredient] *= factor
+    }
+    return scaled
 }
