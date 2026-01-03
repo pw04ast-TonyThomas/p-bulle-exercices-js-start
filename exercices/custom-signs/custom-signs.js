db795928be27be73eab13fb3ffc9084ca48d6788
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-01-03 11:09:33 +0100

    Feat(Exercice): MAJ de l'exercice Custom Signs
    [2] [DONE]

--- exercices/custom-signs/custom-signs.js
+++ exercices/custom-signs/custom-signs.js
@@ -39,8 +39,7 @@ export function buildBirthdaySign(age) {
  */
 
 export function graduationFor(name, year) {
-  return `Congratulations ${name}!
-Class of ${year}`
+  return `Congratulations ${name}!\nClass of ${year}`
 }
 
 /**
