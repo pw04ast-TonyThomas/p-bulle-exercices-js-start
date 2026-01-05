6f64f91a09b2713618acf6fcba6481954082c3ba
Tony Thomas <pw04ast@eduvaud.ch>
2026-01-05 10:53:10 +0100

    Feat(Exercice): Fini l'exercice Gigasecond
    [10] [DONE]

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -3,6 +3,6 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this line and implement the function');
+export const gigasecond = (date) => {
+  return new Date(date.getTime() + 1000000000000)
 };
