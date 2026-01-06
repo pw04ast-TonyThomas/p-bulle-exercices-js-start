1aefba6455ffe8b8be4c48dbf63dba321758f178
pw04ast-TonyThomas <pw04ast@eduvaud.ch>
2026-01-06 09:14:41 +0100

    Feat(Exercice): Fini l'exercice Space Age
    [15] [DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
-};
+const NORMAL_YEAR = 31557600
+
+const PLANETS = {
+  mercury:	0.2408467,
+  venus:	0.61519726,
+  earth:	1.0,
+  mars:	1.8808158,
+  jupiter:	11.862615,
+  saturn:	29.447498,
+  uranus:	84.016846,
+  neptune:	164.79132,
+}
+
+export function age(planet, seconds) {
+  if (!PLANETS[planet]) {
+    throw new Error("not a planet");
+  }
+  return Number((seconds / NORMAL_YEAR / PLANETS[planet]).toFixed(2))
+}
